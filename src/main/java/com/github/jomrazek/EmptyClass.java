130906c99c4005bf7924c6383e5809dbb98e4ff8
Jozef Mrazek <jmrazek@redhat.com>
2021-08-24 11:01:01 +0200

    Add dependency for bacon tests

--- src/main/java/com/github/jomrazek/EmptyClass.java
+++ src/main/java/com/github/jomrazek/EmptyClass.java
@@ -1,5 +1,12 @@
+package com.github.jomrazek;
+
+import lombok.Data;
+
 /**
  * Author: Michal Szynkiewicz, michal.l.szynkiewicz@gmail.com Date: 4/8/16 Time: 3:18 PM
  */
+@Data
 public class EmptyClass {
+
+    private String empty;
 }
